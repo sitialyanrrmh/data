4fa1b82b8cd8b3911f270071a07b593bde7f2e4c
sitialyanrrmh <145677541+sitialyanrrmh@users.noreply.github.com>
2024-10-03 12:51:35 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Fungsi untuk memuat data dari file di komputer (otomatis tanpa upload)
 def load_data(file_path):
-    hour_df = pd.read_csv('https://github.com/sitialyanrrmh/project_analisis_data/blob/5f4e6ceb29ddfb540d650f0df4091c40041649a4/dashboard/main.csv')
+    hour_df = pd.read_csv('file_path')
     
     # Pilih kolom yang diinginkan untuk korelasi
     variables_x = ['temp', 'atemp', 'hum', 'windspeed']  # Sesuaikan nama kolom dengan dataset
